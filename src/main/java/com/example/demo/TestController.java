ce754944dacec902fa89be3ac53acdd0399ccf13
Savva Kodeikin <savva_kodeikin@epam.com>
2019-11-21 18:11:23 +0300

    Eventually fixed

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,55 +1,31 @@
 package com.example.demo;
 
-import java.time.Duration;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 
 @RestController
 public class TestController {
 
-  private Queue<Event> eventQueue = new LinkedBlockingQueue<>();
+  private Consumer<Event> onEventAdded;
 
-  private ConnectableFlux<Object> eventStream;
-
-  private synchronized ConnectableFlux<Object> getCompletableEventStream() {
-    if (eventStream == null) {
-      eventStream = Flux.create(emitter -> {
-        while (true) {
-          Event event = eventQueue.poll();
-          if (event != null) {
-            System.out.println("sending " + event);
-            emitter.next(event);
-          }
-        }
-      })
-          .sample(Duration.ofSeconds(1))
-          .doOnSubscribe(s -> System.out.println("subscribed: " + s))
-          .doOnComplete(() -> System.out.println("complete"))
-          .doOnRequest(c -> System.out.println("on request " + c))
-          .publish();
-
-      eventStream.connect();
-    }
-
-    return eventStream;
-  }
+  private Flux<Object> eventStream = Flux
+      .create(emitter -> onEventAdded = emitter::next)
+      .log()
+      .share();
 
 
   @RequestMapping(path = "/stream", method = RequestMethod.GET, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   public Flux<Object> getEventStream() {
-    return getCompletableEventStream();
+    return eventStream;
   }
 
   @PostMapping("event")
   public void receiveEvent(Event event) {
-    System.out.println(eventQueue.size());
-    eventQueue.add(event);
+    onEventAdded.accept(event);
   }
 }
